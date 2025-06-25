ff69a6c50679c9023c7591a7ec8bb8b197ff2113
Kehinde ODETOLA <odetolakehinde5@gmail.com>
2025-06-26 00:20:59 +0100

    Update URL for Footer Text

--- src/slack/model.go
+++ src/slack/model.go
@@ -4,14 +4,12 @@ const (
 	// HeaderText text to be used in the header of the slack message
 	HeaderText = "Stickers, made for Slack!"
 	// FooterText text to be used in the footer of the slack message
-	FooterText = "powered by slackstickers.com"
+	FooterText = "powered by docs.stickersforslack.com"
 	// ColorText text to be used in the color of the slack message
 	ColorText = "#F26722"
 	// IconURL url to the icon of the slack message
 	IconURL = "https://res.cloudinary.com/slackstickers/image/upload/v1740350898/Slack%20Stickers/slackstickers-logo.svg"
-	// WebsiteURL is the website url
-	WebsiteURL = "slackstickers.com"
 
-	// DocsURL is the doucmentation url
+	// DocsURL is the documentation url
 	DocsURL = "https://docs.slackstickers.com"
 )
